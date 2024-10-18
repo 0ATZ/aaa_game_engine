41dc7d2f2dc7349980578813b26cd7a2e03bea50
Josh Helms <jghelms88@gmail.com>
2024-10-18 01:35:03 -0700

    Overhaul the main game loop to make it much smoother.

--- source/main.cpp
+++ source/main.cpp
@@ -1,27 +1,57 @@
 // Header file for input output functions
 #include <iostream>
 #include <SDL2/SDL.h>
+#include "BaseTypes.h"
 #include "Game.h"
+
+/* Uncomment this line for unreasonably low performance! */
+/* #define LOW_PERFORMANCE_MODE */
 
 using namespace std;
 
 int WinMain(int argc, char* args[]) {
-    Uint32 frameDuration = 1000 / 60; // Frame duration for 60 FPS
+    
     
     Game game;
     if (!game.init()) {
         return 1;
     }
     
+    const T_UINT64 FRAME_DURATION_60FPS = 1000 / 60; // Frame duration for 60 FPS
+    const T_UINT64 FRAME_DURATION_120FPS = 1000 / 120; // Frame duration for 120 FPS
+    const T_UINT64 FRAME_DURATION_140FPS = 1000 / 140; // Frame duration for 140 FPS
+
+    // This variable controls the speed that the game world updates
+    const T_UINT64 UPDATE_DURATION_MS = FRAME_DURATION_60FPS;
+
+    // Adjust this variable to change the frame rate
+    const T_UINT64 RENDER_DURATION_MS = FRAME_DURATION_140FPS;
+
+    // keep track of the last update and render time
+    const T_UINT64 gameStart = SDL_GetTicks64();
+    T_UINT64 prevUpdate = gameStart;
+    T_UINT64 prevRender = gameStart;
     while (game.isRunning())
     {
-        Uint32 startTime = SDL_GetTicks();
-        game.update();
-        game.render();
-        Uint32 frameTime = SDL_GetTicks() - startTime;
-        if (frameDuration > frameTime) {
-            SDL_Delay(frameDuration - frameTime);
+        // perform the game update seperately to keep it consistent
+        if ((SDL_GetTicks64() - prevUpdate) >= UPDATE_DURATION_MS)
+        {
+            prevUpdate = SDL_GetTicks64();
+            game.update();
         }
+
+        // perform the screen render separately to adjust frame rate
+        if ((SDL_GetTicks64() - prevRender) >= RENDER_DURATION_MS)
+        {
+            prevRender = SDL_GetTicks64();
+
+            game.render();
+        }
+#ifdef LOW_PERFORMANCE_MODE
+        // delay makes timing inaccurate due to CPU scheduling overhead
+        // however, delay also significantly decreases CPU usage 
+        SDL_Delay(1);
+#endif
     }
     game.destroy();
     return 0;
